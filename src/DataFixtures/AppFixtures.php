25b3ced0befb3482294d7e4aafed12b3ecdceb81
Dorian Gil <dgil001@iutbayonne.univ-pau.fr>
2019-12-18 19:29:55 +0100

    Création des données de test Stage (Pas encore fonctionnel)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,32 +17,139 @@ class AppFixtures extends Fixture
         $formationDUTInfo = new Formation();
         $formationDUTInfo->setNomLong("Diplôme Universitaire et Technologique Informatique");
         $formationDUTInfo->setNomCourt("DUT Info");
-        $manager->persist($formationDUTInfo);
+
 
         $formationLPMul = new Formation();
         $formationLPMul->setNomLong("Licence Professionnelle Multimédia");
         $formationLPMul->setNomCourt("LP Multimédia");
-        $manager->persist($formationLPMul);
+
 
         $formationDUTIC = new Formation();
         $formationDUTIC->setNomLong("Diplôme Universitaire en Technologies de l'Information et de la Communication");
         $formationDUTIC->setNomCourt("DU TIC");
-        $manager->persist($formationDUTIC);
+
+        $TableauFormation = array($formationDUTInfo, 
+                                  $formationLPMul, 
+                                  $formationDUTIC);
+
+        foreach($TableauFormations as $formation) {
+            $manager->persist($formation);
+        }
 
         //Génération de données de test Entreprise avec Faker dans une boucle itérative
+        /*
         $nbEntreprises = 7;
 
         for ($i = 0 ; $i <= $nbEntreprises ; $i++) {
 
-            $EntrepriseA = new Entreprise();
+            $Entreprise = new Entreprise();
 
-            $EntrepriseA->setNom($faker->regexify('[A-Z][a-z]{5,12}'));
-            $EntrepriseA->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
-            $EntrepriseA->setAdresse($faker->address);
-            $EntrepriseA->setSiteWeb($faker->url);
+            $Entreprise->setNom($faker->regexify('[A-Z][a-z]{5,12}'));
+            $EntrepriseFiducial->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+            $EntrepriseFiducial->setAdresse($faker->address);
+            $EntrepriseFiducial->setSiteWeb($faker->url);            
+        
+            $manager->persist($Entreprise);
+        }*/
 
-            $manager->persist($EntrepriseA);
+        //Génération des données de test Entreprise
+        $EntrepriseFiducial = new Entreprise();
 
+        $EntrepriseFiducial->setNom("Fiducial");
+        $EntrepriseFiducial->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseFiducial->setAdresse($faker->address);
+        $EntrepriseFiducial->setSiteWeb($faker->url);
+
+
+        $EntrepriseCreditAgricole = new Entreprise();
+
+        $EntrepriseCreditAgricole->setNom("Crédit Agricole");
+        $EntrepriseCreditAgricole->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseCreditAgricole->setAdresse($faker->address);
+        $EntrepriseCreditAgricole->setSiteWeb($faker->url);
+
+
+        $EntrepriseCapGemini = new Entreprise();
+
+        $EntrepriseCapGemini->setNom("CapGemini");
+        $EntrepriseCapGemini->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseCapGemini->setAdresse($faker->address);
+        $EntrepriseCapGemini->setSiteWeb($faker->url);
+
+
+        $EntrepriseWellPutt = new Entreprise();
+
+        $EntrepriseWellPutt->setNom("Well Putt");
+        $EntrepriseWellPutt->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseWellPutt->setAdresse($faker->address);
+        $EntrepriseWellPutt->setSiteWeb($faker->url);
+
+
+        $EntrepriseZalando = new Entreprise();
+
+        $EntrepriseZalando->setNom("Zalando");
+        $EntrepriseZalando->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseZalando->setAdresse($faker->address);
+        $EntrepriseZalando->setSiteWeb($faker->url);
+
+
+        $EntrepriseSeriousWeb = new Entreprise();
+
+        $EntrepriseSeriousWeb->setNom("Serious Web");
+        $EntrepriseSeriousWeb->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseSeriousWeb->setAdresse($faker->address);
+        $EntrepriseSeriousWeb->setSiteWeb($faker->url);
+
+
+        $EntrepriseMetaPhylo = new Entreprise();
+
+        $EntrepriseMetaPhylo->setNom("MetaPhylo");
+        $EntrepriseMetaPhylo->setActivite($faker->realText($maxNbChars = 150, $indexSize = 2));
+        $EntrepriseMetaPhylo->setAdresse($faker->address);
+        $EntrepriseMetaPhylo->setSiteWeb($faker->url);
+
+
+        //Tableau d'entreprises
+        $TableauEntreprises = array($EntrepriseCapGemini, 
+                                    $EntrepriseCreditAgricole, 
+                                    $EntrepriseFiducial, 
+                                    $EntrepriseMetaPhylo, 
+                                    $EntrepriseSeriousWeb, 
+                                    $EntrepriseWellPutt, 
+                                    $EntrepriseZalando);
+
+        //Mise en persistance des objets Entreprises
+        foreach($TableauEntreprises as $entreprise) {
+            $manager->persist($entreprise);
+        }
+
+
+        //Génération des données de test Stage
+        $nbStageAGenerer = $faker->numberBetween($min = 0, $max = 8);
+        for ($numStage = 0; $numStage < $nbStageAGenerer; $nbStageAGenerer++) {
+            $Stage = new Stage();
+            $Stage->setTitre($faker->sentence($nbWords = 5, $variableNbWords = true));
+            $Stage->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $Stage->setEmail($faker->companyEmail);
+            
+            //Création de la relation Stage --> Formation
+            $Stage
+
+            //Sélectionne une Formation au hasard
+            $Stage->addFormation($formation);
+
+            //Sélectionner une Entreprise au hasard parmi les 7 dans $TableauEntreprises
+            $numEntreprise = $faker->numberBetween($min = 0, $max = 6);
+
+            //Création relation Stage --> Entreprise
+            $Stage->setEntreprise($TableauEntreprises[$numEntreprise]);
+
+            //Création relation Entreprise --> Stage
+            $TableauEntreprises[$numStage]->addStage($Stage);
+
+            //Persistez les éléments modifiés
+            $manager->persist($Stage);
+            $manager->persist($TableauEntreprises[$numEntreprise]);
         }
 
         //Envoyer les données en BD
