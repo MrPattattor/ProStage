c1384ce13641424c3d73283f37aa69c6cc8c8c11
Dorian GIL <gildorian2000@gmail.com>
2020-02-07 11:31:06 +0100

    Réalisation des pages Entreprise et Formation

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Stage;
+use App\Entity\Entreprise;
+use App\Entity\Formation;
 
 class ProStageController extends AbstractController
 {
@@ -16,7 +18,7 @@ class ProStageController extends AbstractController
         //Récupérer le repository de l'entité Stage
         $repositoryStage = $this->getDoctrine()->getRepository(Stage::class);
 
-        //Récupérer les stage en BD
+        //Récupérer les stages en BD
         $stages = $repositoryStage->findAll();
 
         //Envoyer les stages récupérés à la vue qui a pour but de les afficher
@@ -25,13 +27,24 @@ class ProStageController extends AbstractController
 
     public function indexEntreprises()
     {
+        //Récupérer le repository de l'entité Entreprise
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+
+        //Récupérer les entreprises en BD
+        $entreprises = $repositoryEntreprise->findAll();
         
-        return $this->render('pro_stage/entreprises.html.twig');
+        return $this->render('pro_stage/entreprises.html.twig', ['entreprises'=>$entreprises]);
     }
 
     public function indexFormations()
     {
-        return $this->render('pro_stage/formations.html.twig');
+        //Récupérer le repository de l'entité Formation
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+
+        //Récupérer les formations en BD
+        $formations = $repositoryFormation->findAll();
+
+        return $this->render('pro_stage/formations.html.twig', ['formations'=>$formations]);
     }
 
     public function indexStages($id){
