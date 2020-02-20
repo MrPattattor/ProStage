d5957a65050c69bf042ba9b866e925c063bbfa82
Dorian GIL <gildorian2000@gmail.com>
2020-02-20 11:43:13 +0100

    Finalisation du processus de soumission du formulaire d'ajout d'une Entreprise

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -13,6 +13,8 @@ use App\Repository\FormationRepository;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class ProStageController extends AbstractController
 {
@@ -28,7 +30,7 @@ class ProStageController extends AbstractController
         return $this->render('pro_stage/index.html.twig', ['stages'=>$stages]);
     }
 
-    public function indexAjoutEntreprise()
+    public function indexAjoutEntreprise(Request $request, ObjectManager $manager)
     {
         //Création d'une entreprise vierge qui sera remplie par le formulaire
         $entreprise = new Entreprise();
@@ -40,6 +42,21 @@ class ProStageController extends AbstractController
         ->add('adresse', TextType::class)
         ->add('siteWeb', UrlType::class)
         ->getForm();
+
+        /* On demande au formulaire d'analyser la dernière requête Http. Si le tableau POST contenu dans cette requête contient
+        des variables nom, activite, etc. Alors la méthode handleRequest() recupère les valeurs de ces variables et les
+        affecte à l'objet $entreprise. */
+        $formulaireEntreprise->handleRequest($request);
+
+        if ($formulaireEntreprise->isSubmitted())
+        {
+            //Enregistrer l'entreprise en base de données
+            $manager->persist($entreprise);
+            $manager->flush();
+
+            //Rediriger l'utilisateur vers la page d'accueil
+            return $this->redirectToRoute('pro_stage_accueil');
+        }
 
         //Afficher la page présentant le formulaire d'ajout d'une entreprise
         return $this->render('pro_stage/ajoutEntreprise.html.twig', ['vueFormulaire' => $formulaireEntreprise->createView()]);
