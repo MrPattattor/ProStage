ed8e76001ee588148fa09a0548608f1cbbd46bcf
Dorian GIL <gildorian2000@gmail.com>
2020-03-11 14:03:18 +0100

    Utilisation d'une requête Query Builder dans le formaulaire d'ajout d'un Stage pour lister des formations

--- src/Form/StageType.php
+++ src/Form/StageType.php
@@ -10,6 +10,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use App\Repository\FormationRepository;
 
 class StageType extends AbstractType
 {
@@ -31,6 +32,11 @@ class StageType extends AbstractType
             ->add('formations', EntityType::class, array(
 
                 'class' => Formation::class,
+                'query_builder' => function(FormationRepository $repoFormation){
+                    return $repoFormation->createQueryBuilder('f')
+                        ->andWhere('f.nomCourt LIKE \'DU%\'')
+                        ->orderBy('f.nomCourt', 'ASC');
+                },
                 'choice_label' => function(Formation $formation){
                     return $formation->getNomCourt()." ".$formation->getNomLong();
                 },
